602c61d20fcb434cf4d5d478388b23d714e65b0a
Meena.Hoda <meena.hoda@wmfs.net>
2020-01-27 16:36:15 +0000

    style: Do not access Object.prototype method 'hasOwnProperty' from target object

--- test/example-tests.js
+++ test/example-tests.js
@@ -7,10 +7,10 @@ const expect = chai.expect
 
 describe('Run some schema validation tests', function () {
   it('should check examples are returned', function () {
-    expect(examples.hasOwnProperty('complex')).to.equal(true)
-    expect(examples.hasOwnProperty('expression')).to.equal(true)
-    expect(examples.hasOwnProperty('set')).to.equal(true)
-    expect(examples.hasOwnProperty('simple')).to.equal(true)
-    expect(examples.hasOwnProperty('simpleFormWithBasicProblems')).to.equal(true)
+    expect(Object.prototype.hasOwnProperty.call(examples, 'complex')).to.equal(true)
+    expect(Object.prototype.hasOwnProperty.call(examples, 'expression')).to.equal(true)
+    expect(Object.prototype.hasOwnProperty.call(examples, 'set')).to.equal(true)
+    expect(Object.prototype.hasOwnProperty.call(examples, 'simple')).to.equal(true)
+    expect(Object.prototype.hasOwnProperty.call(examples, 'simpleFormWithBasicProblems')).to.equal(true)
   })
 })
